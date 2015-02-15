f0bd57d6cb12a6146343429d5b832d4053747ee6
adn <adonaac@gmail.com>
2015-02-15 17:11:46 -0200

    Remove loadGamepadMappings for now, add support for multiple joysticks as well as isGamepad checks (for using or not using loadGamepadMappings)

--- Input.lua
+++ Input.lua
@@ -2,9 +2,6 @@ local input_path = tostring(...):sub(1, -6)
 local Input = {}
 Input.__index = Input
 
-local mappings = love.filesystem.read(input_path .. 'gamecontrollerdb.txt')
-if love.joystick.loadGamepadMappings then love.joystick.loadGamepadMappings(mappings) end
-
 local all_keys = {
     "space", "return", "escape", "backspace", "tab", "space", "!", "\"", "#", "$", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "0", "1", "2", "3", "4",
     "5", "6", "7", "8", "9", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "", "`", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
