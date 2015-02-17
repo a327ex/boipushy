1789cc5b4cbaf9f782afd42e591bf47e31afd19d
adn <adonaac@gmail.com>
2015-02-17 19:02:44 -0200

    Allow path.to.input, remove usage of mapping call

--- Input.lua
+++ Input.lua
@@ -13,9 +13,6 @@ while #current_path ~= #input_path do
     end
 end
 
-local mappings = love.filesystem.read(current_path .. 'gamecontrollerdb.txt')
-if love.joystick.loadGamepadMappings then love.joystick.loadGamepadMappings(mappings) end
-
 local all_keys = {
     "space", "return", "escape", "backspace", "tab", "space", "!", "\"", "#", "$", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "0", "1", "2", "3", "4",
     "5", "6", "7", "8", "9", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "", "`", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
